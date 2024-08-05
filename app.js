50ebc575c8658789ccff4926c96791ceffb8c032
Juan Carlos Cruz <j.cruzb@outlook.com>
2024-08-05 11:57:13 +0200

    uptating http to https

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const corsOptions = {
-  origin: 'http://aulacorporativa.com',
+  origin: 'https://aulacorporativa.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
