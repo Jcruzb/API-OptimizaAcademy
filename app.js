5d86cc19aaec8b596e833ba144cd087ec10e8ad7
Juan Carlos Cruz <j.cruzb@outlook.com>
2024-08-05 10:53:58 +0200

    updating cors

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors())
+const corsOptions = {
+  origin: 'http://aulacorporativa.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
+// app.use(cors())
 
 // app.use(cors(
 //   {
